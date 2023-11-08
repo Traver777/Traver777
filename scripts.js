9f0c7804cc07ea09e417654b1c5abb5a066b9a32
Traver777 <92796005+Traver777@users.noreply.github.com>
2023-11-08 15:33:47 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-let carrinho = [];
-
-function adicionarAoCarrinho(produto) {
-  carrinho.push(produto);
-  document.getElementById('carrinho').innerHTML = `<h2>Carrinho:</h2> ${carrinho.join(', ')}`;
-}
